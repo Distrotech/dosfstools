a2e15b2d90a074da9a2be5e6f9084c828fb77f16
Daniel Baumann <mail@daniel-baumann.ch>
2013-07-16 13:58:24 +0200

    Switching license from GPL-3+ back to GPL-2+.
    FIXME: add proper rational why we're doing this

--- src/file.h
+++ src/file.h
@@ -3,21 +3,22 @@
    Copyright (C) 1993 Werner Almesberger <werner.almesberger@lrc.di.epfl.ch>
    Copyright (C) 2008-2013 Daniel Baumann <mail@daniel-baumann.ch>
 
-   This program is free software: you can redistribute it and/or modify
+   This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
+   the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program. If not, see <http://www.gnu.org/licenses/>.
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
    The complete text of the GNU General Public License
-   can be found in /usr/share/common-licenses/GPL-3 file.
+   can be found in /usr/share/common-licenses/GPL-2 file.
 */
 
 #ifndef _FILE_H
